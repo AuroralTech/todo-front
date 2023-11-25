5f487cd4a54696ee0402534961861025f307f698
sako0 <y.m1911a1.0921@gmail.com>
2023-11-25 20:08:18 +0900

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <main className="min-h-screen w-1 p-2"></main>;
+'use client';
+import { useState } from 'react';
+
+export default function Page() {
+  const [todos, setTodos] = useState<string[]>([]);
+  const [input, setInput] = useState('');
+
+  const handleAddTodo = () => {
+    setTodos([...todos, input]);
+    setInput('');
+  };
+
+  return (
+    <main className="min-h-screen w-1 p-2">
+      <h1>Todoアプリ</h1>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={handleAddTodo}>追加</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
