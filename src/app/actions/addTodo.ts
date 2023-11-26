abede0753a490a9528d2b1a39111d1345679bbcf
sako <57947664+sako0@users.noreply.github.com>
2023-11-26 23:33:52 +0900

    [TSK-39] サーバーサイドコンポーネントを試す (#2)

---
+++ src/app/actions/addTodo.ts
@@ -0,0 +1,40 @@
+'use server';
+import { MutationAddTodoArgs, Mutation } from '@/gql/graphql';
+import { GraphQLClient, gql } from 'graphql-request';
+import { revalidatePath } from 'next/cache';
+
+type AddTodoReturn = {
+  message: string;
+};
+
+export const addTodo = async (prevState: AddTodoReturn, formData: FormData) => {
+  const BASE_GRAPHQL_ENDPOINT = 'http://127.0.0.1:4000/graphql';
+  const client = new GraphQLClient(BASE_GRAPHQL_ENDPOINT);
+  formData.append('is_completed', 'false');
+  const task = (formData.get('task') as string) ?? '';
+  const is_completed = false;
+  const variables: MutationAddTodoArgs = {
+    input: {
+      task,
+      is_completed,
+    },
+  };
+
+  const res = await client.request<Mutation, MutationAddTodoArgs>(
+    gql`
+      mutation addTodo($input: TodoItemInput!) {
+        addTodo(input: $input) {
+          id
+          task
+          is_completed
+        }
+      }
+    `,
+    variables
+  );
+  revalidatePath('/todo');
+  if (res) {
+    return { message: `Added to ${res.addTodo.task}` };
+  }
+  return { message: `Failed to add` };
+};
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,35 @@
+import { Query } from '@/gql/graphql';
+import { GraphQLClient, gql } from 'graphql-request';
+import { TodoDeleteForm } from './TodoDeleteForm';
+
+export const TodoList = async () => {
+  const BASE_GRAPHQL_ENDPOINT = 'http://127.0.0.1:4000/graphql';
+  const client = new GraphQLClient(BASE_GRAPHQL_ENDPOINT);
+
+  const res = await client.request<Query>(gql`
+    {
+      todoList {
+        items {
+          id
+          task
+          is_completed
+        }
+      }
+    }
+  `);
+  return (
+    <div>
+      {res &&
+        res.todoList.items.map((todo) => {
+          return (
+            <div key={todo.id} className="mt-3 flex w-auto items-center gap-2 border-b">
+              <p className="w-3">{todo.id}</p>
+              <p className={`flex-grow ${todo.is_completed ? 'line-through' : ''}`}>{todo.task}</p>
+
+              <TodoDeleteForm id={todo.id} todo={todo.task} />
+            </div>
+          );
+        })}
+    </div>
+  );
+};
